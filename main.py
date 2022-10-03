1f9543c01bb92b15608c48ccbf8694aeb52a5acd
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-10-02 21:15:29 -0300

    :wrench: | Formatando quantidade de votos

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import json
 import pandas as pd
 import time
 import os
+import locale
 
+locale.setlocale(locale.LC_ALL, 'pt_BR')
 
-
-def imprimir():
+while True:
+    os.system('cls')
     data = requests.get(
         'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
 
     json_data = json.loads(data.content)
-
     candidato = []
     partido = []
     votos = []
@@ -22,7 +23,12 @@ def imprimir():
         if informacoes['seq'] in ['1', '2', '3', '4']:
             candidato.append(informacoes['nm'])
             partido.append(informacoes['cc'][0:3])
-            votos.append(informacoes['vap'])
+            votos.append(
+            locale.format_string(
+                '%d',
+                int(informacoes['vap']),
+                grouping=True)
+            )
             porcentagem.append(informacoes['pvap'] + ' %')
 
     df_eleicao = pd.DataFrame(list(zip(candidato, partido, votos, porcentagem)), columns=[
@@ -32,7 +38,8 @@ def imprimir():
 
     print(df_eleicao)
     time.sleep(5)
-    os.system('cls')
-    imprimir()
 
-imprimir()
+    # Emulando um do-while
+    if float(json_data['pst'].replace(',', '.')) == float(100):
+        break
+
